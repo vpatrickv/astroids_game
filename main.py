e3544566fe26e9e7c7342ccdaa754598f2dd9b68
vpatrickv <patrickdelange68@gmail.com>
2025-05-09 00:15:41 +0200

    added game window and close button

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
+
+
+
 
 if __name__ == "__main__":
     main()
