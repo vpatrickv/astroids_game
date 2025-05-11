6949a51ca19be1ccda793ec619285726725c9a40
vpatrickv <patrickdelange68@gmail.com>
2025-05-11 11:36:45 +0200

    limited gameloop to 60fps and added sprites

--- main.py
+++ main.py
@@ -5,22 +5,20 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
-
-
-
+        
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
